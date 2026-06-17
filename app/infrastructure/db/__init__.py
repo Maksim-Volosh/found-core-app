d93f139530916fa107edcd0fc4a928312ffdbf4c
Maksim Volosh <maksgoza9@gmail.com>
2026-06-17 11:25:01 +0300

    feat: setup db_helper using postgresql and asyncpg

---
+++ app/infrastructure/db/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "db_helper",
+]
+
+from app.infrastructure.db.db_helper import db_helper
--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
-import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.core.settings import settings
 from app.api.v1 import api_v1_router
+from app.core.config import settings
+from app.infrastructure.db import db_helper
+from app.infrastructure.models import Base
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Initialize
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
     yield  # ---------
 
     # Cleanup
+    await db_helper.dispose()
 
 
 
