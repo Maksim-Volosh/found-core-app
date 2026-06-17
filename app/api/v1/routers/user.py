7c304eb0fe7ec60d1212186811affb70cbc5d089
Maksim Volosh <maksgoza9@gmail.com>
2026-06-17 20:06:10 +0300

    feat: create user route and schemas

--- app/api/v1/routers/user.py
+++ app/api/v1/routers/user.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, HTTPException
 
-from app.api.v1.schemas.user  import UserRequest, UserResponse
+from app.api.v1.schemas.user import UserRequest, UserResponse
+from app.core.composition.container import Container
+from app.core.composition.di import get_container
+from app.domain.exceptions import UserNotFoundByTelegramId
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -8,12 +11,23 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/users/auth")
 async def auth_user(
     user: UserRequest,
+    container: Container = Depends(get_container),
 ) -> UserResponse:
-    return UserResponse(
-        user_id=1,
-        telegram_id=user.telegram_id,
-        username="username",
-        first_name="first_name",
-        level=1,
-        is_banned=False
-    )
+    """
+    Authenticate a user by their Telegram ID.
+
+    Args:
+        user (UserRequest): The user request containing the Telegram ID.
+        container (Container): The dependency injection container.
+
+    Returns:
+        UserResponse: The authenticated user's information.
+
+    Raises:
+        HTTPException: If the user is not found.
+    """
+    try:
+        user_entity = await container.user_use_case().get_by_telegram_id(user.telegram_id)
+        return UserResponse.model_validate(user_entity, from_attributes=True)
+    except UserNotFoundByTelegramId as e:
+        raise HTTPException(status_code=404, detail=e.message)
--- app/api/v1/schemas/user.py
+++ app/api/v1/schemas/user.py
@@ -5,11 +5,15 @@ from pydantic import BaseModel
 
 class UserRequest(BaseModel):
     telegram_id: int
+    username: Optional[str] = None
+    first_name: Optional[str] = None
 
 class UserResponse(BaseModel):
     user_id: int
     telegram_id: int
     username: str
     first_name: str
+    last_name: Optional[str]
     level: int
     is_banned: bool
+    is_admin: bool
