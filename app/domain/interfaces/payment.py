629c42ec376da0c8d5b2429358996863520f9953
Maksim Volosh <maksgoza9@gmail.com>
2026-06-25 04:23:34 +0300

    feat: change interfaces for unit of work

--- app/domain/interfaces/payment.py
+++ app/domain/interfaces/payment.py
@@ -6,11 +6,19 @@ from app.domain.entities.payment import PaymentStatus
 
 class IPaymentRepository(ABC):
     @abstractmethod
+    async def commit(self) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
     async def create_payment(self, payment: NewPaymentEntity) -> None:
         raise NotImplementedError
     
     @abstractmethod
     async def get_pending_payment(self, user_id: int) -> PaymentEntity | None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def get_by_provider_payment_id(self, provider_payment_id: str) -> PaymentEntity | None:
         raise NotImplementedError
     
     @abstractmethod
--- app/domain/interfaces/subscription.py
+++ app/domain/interfaces/subscription.py
@@ -4,5 +4,13 @@ from app.domain.entities import SubscriptionEntity
 
 class ISubscriptionRepository(ABC):
     @abstractmethod
+    async def commit(self) -> None:
+        raise NotImplementedError
+    
+    @abstractmethod
     async def get_subscription(self, user_id: int) -> SubscriptionEntity | None:
+        raise NotImplementedError
+    
+    @abstractmethod
+    async def create_subscription(self, subscription: SubscriptionEntity) -> None:
         raise NotImplementedError
